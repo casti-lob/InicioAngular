a2c27d34229cd935475154ab0f6ad794b128378d
casti-lob <casti_lob@hotmail.com>
2022-11-11 19:52:03 +0100

    terminado brawl-stars

--- src/app/brawl-stars/main-page/main-page.component.ts
+++ src/app/brawl-stars/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../interfaces/Player';
 
 @Component({
   selector: 'app-main-page',
@@ -6,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  players =[
-    { name: 'Shelly', health: 3600},
-    { name: 'Nita', health: 3800},
-    { name: 'Colt', health: 2800}
-]
   constructor() { }
-
   ngOnInit(): void {
   }
+  players : Player[]=[
+    { name: 'Shelly', health: 3600},
+    { name: 'Nita', health: 3800},
+    { name: 'Colt', health: 2800},
+    { name: 'Ciry', health: 8000},
+    { name: 'Gerald', health: 1200},
+    { name: 'Triss', health: 800},
+    { name: 'Yenny', health: 2100},
+    { name: 'Arthur', health: 3100},
+]
+  
+
+ 
 
 }
